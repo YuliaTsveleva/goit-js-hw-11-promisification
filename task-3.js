aaac6f7522c20df50ce602b6fda78b996d4d532a
YuliaTsveleva <tsveleva.yulia@gmail.com>
2021-10-16 17:49:56 +0300

    rest

--- task-3.js
+++ task-3.js
@@ -10,14 +10,14 @@ const makeTransaction = (transaction) => {
 
     setTimeout(() => {
       if (canProcess) {
-        resolve(transaction.id, delay);
+        resolve({ id: transaction.id, time: delay });
       }
       reject(transaction.id);
     }, delay);
   });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
